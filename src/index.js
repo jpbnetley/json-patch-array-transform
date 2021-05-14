bf4932503e76f0552854974fdd14c604e3292af8
Jonathan Netley <jpbnetley@gmail.com>
2021-05-14 09:14:34 +0200

    function improvement

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import "./styles.css";
 
-const oridginal = {
+const original = {
   name: "personName",
   pets: ["cat", "dog"],
   surname: "net",
@@ -8,17 +8,23 @@ const oridginal = {
 };
 const update = { name: "personName", kids: ["Jack"] };
 
-const arrayNames = Object.keys(oridginal).filter((key) =>
-  Array.isArray(oridginal[key])
+const fillArray = (original = {}, update = {}) => {
+  const arrayNames = Object.keys(original).filter((key) =>
+    Array.isArray(original[key])
+  );
+
+  const item = Object.keys(update).reduce((currentObj, currentKey) => {
+    const arrNameLength = arrayNames.filter((arr) => arr === currentKey).length;
+
+    if (arrNameLength && Array.isArray(update[currentKey])) {
+      return { ...currentObj, [currentKey]: [] };
+    } else {
+      return { ...currentObj, [currentKey]: update[currentKey] };
+    }
+  });
+
+  return item;
+};
+document.getElementById("app").innerHTML = JSON.stringify(
+  fillArray(original, update)
 );
-
-const item = Object.keys(update).reduce((currentObj, currentKey) => {
-  const arrNameLength = arrayNames.filter((arr) => arr === currentKey).length;
-  if (arrNameLength && Array.isArray(update[currentKey])) {
-    return { ...currentObj, [currentKey]: [] };
-  } else {
-    return { ...currentObj, [currentKey]: update[currentKey] };
-  }
-});
-
-document.getElementById("app").innerHTML = JSON.stringify(item);
