c5e21788a78f4cdab4a028ccb16a11c5b5e3680e
Jonathan Netley <jpbnetley@gmail.com>
2021-05-14 10:12:28 +0200

    filter array improvement

--- src/index.js
+++ src/index.js
@@ -7,29 +7,57 @@ const original = {
   kids: ["Jack", "James"],
   age: 5
 };
+
+//pets should be removed, so {...pets: [], surname}
 const update = { name: "personName", kids: ["Jack"], age: 8 };
 
+/**
+ * Fills the removed arrays with a empty array
+ * @param {*} original that needs to be updated
+ * @param {*} update valus returned form the FE
+ */
 const fillArray = (original = {}, update = {}) => {
   const arrayNames = Object.keys(original).filter((key) =>
     Array.isArray(original[key])
   );
 
-  const item = Object.keys(update).reduce((currentObj, currentKey) => {
-    const arrNameLength = arrayNames.filter((arr) => arr === currentKey).length;
+  const originalKeys = Object.keys(original);
+  const updateKeys = Object.keys(update);
 
-    const validatedCurrentObject =
+  const difference = originalKeys.filter((x) => !updateKeys.includes(x));
+  const missingArrays = arrayNames.filter((x) => !difference.includes(x));
+  console.log(missingArrays);
+  const filledArrays = missingArrays.reduce((currentObj, item) => {
+    console.log("inLoop", { [item]: [] });
+    const obj =
       currentObj instanceof Object
-        ? { ...currentObj }
-        : { [currentObj]: original[currentObj] };
+        ? { ...currentObj, [item]: [] }
+        : { [item]: [] };
 
-    if (arrNameLength && Array.isArray(update[currentKey])) {
-      return { ...validatedCurrentObject, [currentKey]: [] };
-    } else {
-      return { ...validatedCurrentObject, [currentKey]: update[currentKey] };
-    }
+    console.log(obj);
+    return obj;
   });
 
-  return item;
+  console.log("filledArays", filledArrays);
+
+  return filledArrays;
+
+  // const item = Object.keys(update).reduce((currentObj, currentKey) => {
+  //   const arrNameLength = arrayNames.filter((arr) => arr === currentKey).length;
+
+  //   const validatedCurrentObject =
+  //     currentObj instanceof Object
+  //       ? { ...currentObj }
+  //       : { [currentObj]: original[currentObj] };
+
+  //   if (arrNameLength && Array.isArray(update[currentKey])) {
+  //     return { ...validatedCurrentObject, [currentKey]: [] };
+  //   } else {
+  //     return { ...validatedCurrentObject, [currentKey]: update[currentKey] };
+  //   }
+  // });
+
+  // return item;
 };
 document.getElementById("app").innerHTML = JSON.stringify(
   fillArray(original, update)
