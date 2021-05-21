6ee3ea074b5b80f3221bac75157d7ef13520fde2
Jonathan Netley <jpbnetley@gmail.com>
2021-05-21 13:49:22 +0200

    added more tests

--- src/fillAray.test.js
+++ src/fillAray.test.js
@@ -1,176 +1,186 @@
 import fillArray from "./utils/fillAray";
 
-describe("test Fill Array", () => {
-  test("see if shallow item gets updated, result shows update", () => {
+describe('test Fill Array', () => {
+  test('see if shallow item gets updated, result shows update', () => {
     const original = {
-      name: "personName",
-      pets: ["cat", "dog"],
-      surname: "net",
-      kids: ["Jack", "James"]
-    };
+      name: 'personName',
+      pets: ['cat', 'dog'],
+      surname: 'net',
+      kids: ['Jack', 'James']
+    }
 
     const update = {
-      name: "John",
-      pets: ["cat", "dog"],
-      surname: "net",
-      kids: ["Jack", "James"]
-    };
+      name: 'John',
+      pets: ['cat', 'dog'],
+      surname: 'net',
+      kids: ['Jack', 'James']
+    }
 
     const result = {
-      name: "John",
-      pets: ["cat", "dog"],
-      surname: "net",
-      kids: ["Jack", "James"]
-    };
+      name: 'John',
+      pets: ['cat', 'dog'],
+      surname: 'net',
+      kids: ['Jack', 'James']
+    }
 
-    expect(fillArray(original, update)).toEqual(result);
-  });
+    expect(fillArray(original, update)).toEqual(result)
+  })
 
-  test("see if shallow array that got removed, gets replaced with an empty array", () => {
+  test('see if shallow array that got removed, gets replaced with an empty array', () => {
     const original = {
-      name: "personName",
-      pets: ["cat", "dog"],
-      surname: "net",
-      kids: ["Jack", "James"]
-    };
+      name: 'personName',
+      pets: ['cat', 'dog'],
+      surname: 'net',
+      kids: ['Jack', 'James']
+    }
 
-    const update = { name: "personName", kids: ["Jack"] };
+    const update = { name: 'personName', kids: ['Jack'] }
 
-    const result = { name: "personName", kids: ["Jack"], pets: [] };
+    const result = { name: 'personName', kids: ['Jack'], pets: [] }
 
-    expect(fillArray(original, update)).toEqual(result);
-  });
+    expect(fillArray(original, update)).toEqual(result)
+  })
 
-  test("see if deep array that got removed, by 1 element, still patches 1 element of the array", () => {
+  test('see if deep array that got removed, by 1 element, still patches 1 element of the array', () => {
     const original = {
-      name: "personName",
+      name: 'personName',
       pets: [
-        { name: "cat1", kittens: ["lew", "Chris"] },
-        { name: "cat12", kittens: ["Denice", "Jamie"] }
+        { name: 'cat1', kittens: ['lew', 'Chris'] },
+        { name: 'cat12', kittens: ['Denice', 'Jamie'] }
       ],
-      surname: "net",
-      kids: ["Jack", "James"]
-    };
+      surname: 'net',
+      kids: ['Jack', 'James']
+    }
 
     const update = {
-      name: "personName",
-      pets: [{ name: "cat1", kittens: ["lew", "Chris"] }],
-      kids: ["Jack", "James"]
-    };
+      name: 'personName',
+      pets: [{ name: 'cat1', kittens: ['lew', 'Chris'] }],
+      kids: ['Jack', 'James']
+    }
 
     const result = {
-      name: "personName",
-      pets: [{ name: "cat1", kittens: ["lew", "Chris"] }],
-      kids: ["Jack", "James"]
-    };
+      name: 'personName',
+      pets: [{ name: 'cat1', kittens: ['lew', 'Chris'] }],
+      kids: ['Jack', 'James']
+    }
 
-    expect(fillArray(original, update)).toEqual(result);
-  });
+    expect(fillArray(original, update)).toEqual(result)
+  })
 
-  test("see if shallow array with objects that got removed, produces empty array", () => {
+  test('see if shallow array with objects that got removed, produces empty array', () => {
     const original = {
-      name: "John",
+      name: 'John',
+      surname: 'Doe',
+      kids: ['Jack', 'James'],
       pets: [
-        { name: "cat1", kittens: ["lew", "Chris"] },
-        { name: "cat12", kittens: ["Denice", "Jamie"] }
-      ],
-      surname: "Doe",
-      kids: ["Jack", "James"]
-    };
+        { name: 'cat1', kittens: ['lew', 'Chris'] },
+        { name: 'cat12', kittens: ['Denice', 'Jamie'] }
+      ]
+    }
 
-    const update = { name: "Jane", kids: ["Jack"] };
+    const update = { name: 'Jane', kids: ['Jack'] }
 
-    const result = { name: "Jane", kids: ["Jack"], pets: [] };
+    const result = { name: 'Jane', kids: ['Jack'], pets: [] }
 
-    expect(fillArray(original, update)).toEqual(result);
-  });
+    expect(fillArray(original, update)).toEqual(result)
+  })
 
-  test("see if deep array that got removed, gets replaced with an empty array", () => {
+  test('see if deep array that got removed, gets replaced with an empty array', () => {
     const original = {
-      name: "John",
-      pets: ["cat", "dog"],
-      surname: "net",
-      kids: ["Jack", "James"],
+      name: 'John',
+      surname: 'net',
+      kids: ['Jack', 'James'],
+      pets: ['cat', 'dog'],
+      age: 5,
       deps: {
-        name: "Kat",
+        name: 'Kat',
         arr: [1, 2, 3]
-      },
-      age: 5
-    };
-    //deps.array should be removed, so {...deps.array : []}
-    const update = {
-      name: "John",
-      kids: ["Jack"],
-      pets: ["cat", "dog"],
-      surname: "net",
-      age: 8,
-      deps: {
-        name: "Kat"
       }
-    };
-    const result = {
-      name: "John",
-      kids: ["Jack"],
+    }
+    // deps.array should be removed, so {...deps.array : []}
+    const update = {
+      name: 'John',
+      surname: 'net',
+      kids: ['Jack'],
+      pets: ['cat', 'dog'],
       age: 8,
-      pets: ["cat", "dog"],
-      surname: "net",
       deps: {
-        name: "Kat",
+        name: 'Kat'
+      }
+    }
+    const result = {
+      name: 'John',
+      surname: 'net',
+      kids: ['Jack'],
+      pets: ['cat', 'dog'],
+      age: 8,
+      deps: {
+        name: 'Kat',
         arr: []
       }
-    };
+    }
 
-    expect(fillArray(original, update)).toEqual(result);
-  });
+    expect(fillArray(original, update)).toEqual(result)
+  })
 
-  test("see if shalow and deep array that got removed, gets replaced with an empty array", () => {
+  test('see if shallow and deep array that got removed, gets replaced with an empty array', () => {
     const original = {
-      name: "John",
-      pets: ["cat", "dog"],
-      surname: "net",
-      kids: ["Jack", "James"],
+      name: 'John',
+      surname: 'net',
+      pets: ['cat', 'dog'],
+      kids: ['Jack', 'James'],
       deps: {
-        name: "Kat",
+        name: 'Kat',
         arr: [1, 2, 3]
       },
       age: 5
-    };
-    //deps.array should be removed, so {...deps.array : []}
+    }
+    // deps.array should be removed, so {...deps.array : []}
     const update = {
-      name: "John",
-      kids: ["Jack"],
-      surname: "net",
+      name: 'John',
+      surname: 'net',
+      kids: ['Jack'],
       age: 8,
       deps: {
-        name: "Kat"
+        name: 'Kat'
       }
-    };
+    }
     const result = {
-      name: "John",
-      kids: ["Jack"],
-      age: 8,
+      name: 'John',
+      surname: 'net',
+      kids: ['Jack'],
       pets: [],
-      surname: "net",
+      age: 8,
       deps: {
-        name: "Kat",
+        name: 'Kat',
         arr: []
       }
-    };
+    }
 
-    expect(fillArray(original, update)).toEqual(result);
-  });
+    expect(fillArray(original, update)).toEqual(result)
+  })
 
-  test("see if all patch operations gets sent for a nested object, add 1 item", () => {
-    const original = { name: "jack", kid: { name: "james" } };
-    const update = {
-      name: "jack",
-      kid: { name: "james", surname: "Pieterse" }
-    };
-    const result = {
-      name: "jack",
-      kid: { name: "james", surname: "Pieterse" }
-    };
+  test('see if item gets removed', () => {
+    const original = { name: 'jack', kid: { name: 'james', surname: 'Pieterse' } }
+    const update = { name: 'jack', kid: { name: 'james' } }
+    const result = { name: 'jack', kid: { name: 'james' } }
 
-    expect(fillArray(original, update)).toEqual(result);
-  });
-});
+    expect(fillArray(original, update)).toEqual(result)
+  })
+
+  test('see if a new item is added, if it is included in the result', () => {
+    const original = { name: 'jack' }
+    const update = { name: 'jack', kid: 'james' }
+    const result = { name: 'jack', kid: 'james' }
+
+    expect(fillArray(original, update)).toEqual(result)
+  })
+
+  test('see if all patch operations gets sent for a nested object, add 1 item', () => {
+    const original = { name: 'jack', kid: { name: 'james' } }
+    const update = { name: 'jack', kid: { name: 'james', surname: 'Pieterse' } }
+    const result = { name: 'jack', kid: { name: 'james', surname: 'Pieterse' } }
+
+    expect(fillArray(original, update)).toEqual(result)
+  })
+})
